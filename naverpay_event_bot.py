5892fe6dac510f120c63c9dd60b3a72e8b81b201
Jihoon Kim <jhoon.kim@gmail.com>
2022-04-02 22:19:59 +0900

    Replace image diff with template matching

--- naverpay_event_bot.py
+++ naverpay_event_bot.py
@@ -1,3 +1,11 @@
+import cv2
+import glob
+import numpy as np
+import os
+import pyperclip
+import time
+import tqdm
+
 from urllib import request
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -5,13 +13,6 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 
-from time import sleep
-
-import cv2
-import pyperclip
-import numpy as np
-import glob
-import tqdm
 
 class NaverPayEventBot:
     def __init__(self):
@@ -20,19 +21,10 @@ class NaverPayEventBot:
         self.load_click_event_images()
         self.set_chrome_driver()
          
-    def get_roi_image(self, image):
-        roi_x = 30
-        roi_y = 32
-        roi_w = 84
-        roi_h = 78
-
-        return image[roi_y:roi_y+roi_h, roi_x:roi_x+roi_w]
-
     def load_click_event_images(self):
-        for path in glob.glob('click_event_*.png'):
+        for path in glob.glob(os.path.join('images', 'click_event_*.png')):
             image = cv2.imread(path, cv2.IMREAD_COLOR)
-            roi_image = self.get_roi_image(image)
-            self.click_event_images.append(roi_image)
+            self.click_event_images.append(image)
         
     def set_chrome_driver(self):
         chrome_options = webdriver.ChromeOptions()
@@ -56,13 +48,11 @@ class NaverPayEventBot:
         self.driver.find_element(by=By.ID, value='log.login').click()
 
     def is_click_event(self, img):
-        roi_img = self.get_roi_image(img)
 
         for template in self.click_event_images:
-            diff = np.abs(template - roi_img)
-            match_count = np.count_nonzero(diff < 5)
-            #print(f'score: {match_count}')
-            if match_count > roi_img.shape[0] * roi_img.shape[1] * 3 * 0.9:
+            result = cv2.matchTemplate(img, template, cv2.cv2.TM_SQDIFF_NORMED)
+            box_loc = np.where(result < 1e-5)
+            if len(box_loc[0]) > 0:
                 return True
         
         return False
@@ -73,16 +63,27 @@ class NaverPayEventBot:
             for element in tqdm.tqdm(elements, desc='Finding events'):
                 try:
                     banner_element = element.find_element(by=By.CLASS_NAME, value='banner')
-                    title_element = element.find_element(by=By.CLASS_NAME, value='title')
-                    link_element = element.find_element(by=By.CLASS_NAME, value='type_system')
+
+                    try:
+                        title_element = element.find_element(by=By.CLASS_NAME, value='title')
+                        title = title_element.text
+                        is_image_banner = False
+                    except:
+                        title = "IMAGE BANNER"
+                        is_image_banner = True
+
+                    if is_image_banner is False:
+                        link_element = element.find_element(by=By.CLASS_NAME, value='type_system')
+                    else:
+                        link_element = element.find_element(by=By.CLASS_NAME, value='type_image')
 
                     res = request.urlopen(banner_element.get_attribute('src')).read()
                     img = cv2.imdecode(np.fromstring(res, dtype=np.uint8), cv2.IMREAD_COLOR)
                     url = link_element.get_attribute('href')
 
-                    #print(title_element.text)
+                    #print(title)
                     if self.is_click_event(img):
-                        self.events.append((title_element.text, url))
+                        self.events.append((title, url))
 
                 except Exception as e:
                     pass
@@ -95,18 +96,18 @@ class NaverPayEventBot:
             except:
                 pass
 
-            sleep(3)
+            time.sleep(3)
 
     def click_daily_events(self):
         for url in open('event_urls.txt'):
             print(f'trying {url}')
             self.driver.get(url)
-            sleep(3)
+            time.sleep(3)
 
             try:
                 self.driver.find_element(by=By.CLASS_NAME, value='call_to_action').click()
             except:
                 pass
 
-            sleep(1)
+            time.sleep(1)
     
