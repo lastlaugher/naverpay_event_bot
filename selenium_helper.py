412f688cb5f2b633ffe0eb3d8d6af6db5f603204
Jihoon Kim <jhoon.kim@gmail.com>
2024-07-30 23:29:42 +0900

    Fix selenium error

--- selenium_helper.py
+++ selenium_helper.py
@@ -1,19 +1,16 @@
 
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.chrome.service import Service as ChromeService
-from webdriver_manager.chrome import ChromeDriverManager
 
 import pyperclip
 
 class SeleniumHelper:
     def __init__(self):
         chrome_options = webdriver.ChromeOptions()
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver = webdriver.Chrome(options=chrome_options)
 
         self.driver = driver
 
